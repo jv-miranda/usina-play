5154622151b9c5205b0df10d0517c2602e268797
João Vitor Miranda <joao.vitor.miranda.siqueira@gmail.com>
2025-02-17 17:39:49 -0300

    Tech test finished

--- src/app/services/firebase-requests.service.ts
+++ src/app/services/firebase-requests.service.ts
@@ -11,8 +11,8 @@ export class FirebaseRequestsService {
   /**
    * Recupera uma coleção do Firestore como um Observable do tipo T[].
    *
-   * @param collectionName O nome da coleção no Firestore.
-   * @param queryFn Função de consulta opcional para filtrar ou ordenar a coleção.
+   * @param collectionName - O nome da coleção no Firestore.
+   * @param queryFn - Função de consulta opcional para filtrar ou ordenar a coleção.
    * @returns Um Observable que emite um array do tipo T.
    *
    */
@@ -21,7 +21,9 @@ export class FirebaseRequestsService {
     queryFn?: QueryFn
   ): Observable<T[]> {
     return runInInjectionContext(this.injector, () =>
-      this.afs.collection<T>(collectionName, queryFn).valueChanges()
+      this.afs
+        .collection<T>(collectionName, queryFn)
+        .valueChanges({ idField: 'id' })
     );
   }
 }
